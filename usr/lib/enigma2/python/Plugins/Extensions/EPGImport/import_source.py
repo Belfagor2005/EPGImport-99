f0401e203f6a7ea83d016768d6dd3e199c3769e3
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-03-05 16:22:22 +0100

    Update import_source.py

--- usr/lib/enigma2/python/Plugins/Extensions/EPGImport/import_source.py
+++ usr/lib/enigma2/python/Plugins/Extensions/EPGImport/import_source.py
@@ -9,13 +9,26 @@
 # Info corvoboys.org
 """
 
-from os import listdir, makedirs, chdir, remove, walk, sync
+from os import listdir, makedirs, chdir, remove, walk
 from os.path import join, isdir, exists
-from shutil import rmtree
+from shutil import rmtree, copyfileobj, copytree, copy2
 import tarfile
-import shutil
-import urllib.request
 import ssl
+
+
+def make_dirs(directory):
+	"""Create directory if it does not exist (compatible with Python 2 and 3)."""
+	try:
+		makedirs(directory)
+	except OSError:
+		if not isdir(directory):
+			raise
+
+
+try:
+	import urllib.request as urllib_request  # Python 3
+except ImportError:
+	import urllib2 as urllib_request  # Python 2
 
 
 def main(url):
@@ -23,14 +36,14 @@ def main(url):
 	dest_dir = "/etc/epgimport"
 	SETTINGS_FILE = "/etc/enigma2/epgimport.conf"
 
-	makedirs(TMPSources, exist_ok=True)
-	makedirs(dest_dir, exist_ok=True)
+	make_dirs(TMPSources)
+	make_dirs(dest_dir)
 
 	chdir(TMPSources)
 	tarball = "main.tar.gz"
 	context = ssl._create_unverified_context()
-	with urllib.request.urlopen(url, context=context) as response, open(tarball, "wb") as out_file:
-		shutil.copyfileobj(response, out_file)
+	with urllib_request.urlopen(url, context=context) as response, open(tarball, "wb") as out_file:
+		copyfileobj(response, out_file)
 
 	# Remove existing files in dest_dir before extracting
 	for item in listdir(dest_dir):
@@ -58,14 +71,18 @@ def main(url):
 	for item in listdir(extracted_dir):
 		src_item = join(extracted_dir, item)
 		if isdir(src_item):
-			shutil.copytree(src_item, join(dest_dir, item), dirs_exist_ok=True)
+			copytree(src_item, join(dest_dir, item), dirs_exist_ok=True)
 		else:
-			shutil.copy2(src_item, dest_dir)
+			copy2(src_item, dest_dir)
 
-	shutil.rmtree(TMPSources, ignore_errors=True)
+	rmtree(TMPSources, ignore_errors=True)
 	if exists(SETTINGS_FILE):
 		remove(SETTINGS_FILE)
-	sync()
+	try:
+		from os import sync
+		sync()
+	except ImportError:
+		pass
 
 
 if __name__ == "__main__":
